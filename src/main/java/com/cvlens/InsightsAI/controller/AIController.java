307986ade40f6e7dfe522fa30e83481dca222653
jHallsG <134239144+jHallsG@users.noreply.github.com>
2025-03-28 22:21:49 +0800

    feat(service) : processJobDescription method done

--- src/main/java/com/cvlens/InsightsAI/controller/AIController.java
+++ src/main/java/com/cvlens/InsightsAI/controller/AIController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class AIController {
 		this.aiService = aiService;
 	}
 	
-	@PostMapping("/description")
+	@GetMapping("/description")
 	public ResponseEntity<String> uploadDescription (@RequestParam("job-description") String jd) {
 		
 		return ResponseEntity.status(HttpStatus.OK).body(aiService.processJobDescription(jd));
--- src/main/java/com/cvlens/InsightsAI/service/impl/AIServiceImpl.java
+++ src/main/java/com/cvlens/InsightsAI/service/impl/AIServiceImpl.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -34,7 +38,34 @@ public class AIServiceImpl implements AIServices{
 	@Override
 	public String processJobDescription(String jobDescription) {
 		
-		return "yolo";
+		String resumeContent = jdbcTemplate.queryForObject("SELECT content FROM vector_store", String.class);
+		
+		Message systemMessage = new SystemMessage(""
+				+ "You are an AI-powered resume analysis assistant. Your task is to evaluate resumes against a given job description, highlighting strengths, identifying gaps, and suggesting improvements.\r\n"
+				+ "\r\n"
+				+ "Analyze work experience, technical skills, and education, ensuring responses are factual, structured, and neutral. Do not assume missing details; instead, point them out.\r\n"
+				+ "\r\n"
+				+ "At the end, provide a rating from 1 to 10, where:\r\n"
+				+ "\r\n"
+				+ "1 = Very poor match (few/no relevant qualifications).\r\n"
+				+ "\r\n"
+				+ "5 = Partial match (some relevant skills, key gaps remain).\r\n"
+				+ "\r\n"
+				+ "8 = Strong match (meets most requirements, minor gaps).\r\n"
+				+ "\r\n"
+				+ "10 = Perfect match (fully aligned with job needs).\r\n"
+				+ "\r\n"
+				+ "Justify your rating with specific reasons based on the resume's content."
+				+ "\r\n"
+				+ "The resume content are as follows : \r\n\n\n" + resumeContent);
+		
+		Message jobDescriptionContent = new UserMessage(jobDescription);
+		
+		Prompt prompts = new Prompt(systemMessage, jobDescriptionContent);
+		
+		String result = chatModel.call(prompts).getResult().getOutput().getText();
+		
+		return result;
 	}
 
 	@Override
